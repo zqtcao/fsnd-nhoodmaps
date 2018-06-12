c00e532c6624cbf13268dbcbf4e4b7fb023c0a5c
Tom Cao <zqt.cao91@gmail.com>
2018-06-12 01:06:50 -0700

    add location toggling and filtering

--- js/app.js
+++ js/app.js
@@ -10,16 +10,54 @@ function MapLocation(name, id, isFavorite = false) {
 
 function MapsViewModel() {
 	var self = this;
-	var markers = [];
-	self.mapLocList = ko.observableArray();
-	
+    
+	self.markers = [];
+    self.largeInfowindow = new google.maps.InfoWindow();
+    self.searchFilter = ko.observable("");
+    		
+    this.populateInfoWindow = function(marker, infowindow) {
+
+        //Foursquare API endpoint
+        var fsPlaceUrl = "https://api.foursquare.com/v2/venues/search?ll=" 
+            + marker.lat + "," + marker.lng
+            + "&query=" + marker.title
+            + "&client_id=" + fsClientID
+            + "&client_secret=" + fsClientSecret
+            + "&limit=1&v=20180609";
+        console.log(fsPlaceUrl);
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+            infowindow.marker = marker;
+
+            //Foursquare API call
+            $.getJSON(fsPlaceUrl, function(marker) {
+                var venueResponse = marker.response.venues[0];
+                var locName = venueResponse.name;
+                var locAddressLine1 = venueResponse.location.formattedAddress[0];
+                var locAddressLine2 = venueResponse.location.formattedAddress[1];
+
+                infowindow.setContent('<div><h4>' + locName + '</h4>' 
+                + '<h5>' + locAddressLine1 + '</h5><h5>' + locAddressLine2 + '</h5>'
+                + '<br><img src="img/Powered-by-Foursquare-full-color-300.png"></div>');
+            }).fail(function(e) {
+                infowindow.setContent('<div>' + fsErr + '</div>');
+            });
+            infowindow.open(map, marker);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick',function() {
+                infowindow.setMarker = null;
+            });
+        }
+    }
+
+    
 	this.initMap = function() {
 		map = new google.maps.Map(document.getElementById('map'), {
 			center: {lat: 34.0618214, lng: -118.0786024},
 			styles: styles,
 			zoom: 14
 		});
-		var largeInfowindow = new google.maps.InfoWindow();
+		
 		var bounds = new google.maps.LatLngBounds();
 		// The following group uses the location array to create an array of markers on initialize.
 		for (var i = 0; i < places.length; i++) {
@@ -28,7 +66,6 @@ function MapsViewModel() {
 			var title = places[i].title;
 			var lat = position.lat;
 			var lng = position.lng;
-			this.mapLocList.push(new MapLocation(title, i))
 			// Create a marker per location, and put into markers array.
 			var marker = new google.maps.Marker({
 				map: map,
@@ -40,50 +77,49 @@ function MapsViewModel() {
 				id: i
 			});
 			// Push the marker to our array of markers.
-			markers.push(marker);
+			self.markers.push(marker);
 			// Create an onclick event to open an infowindow at each marker.
 			marker.addListener('click', function() {
-				populateInfoWindow(this, largeInfowindow);
+				self.populateInfoWindow(this, self.largeInfowindow);
 			});
-			bounds.extend(markers[i].position);
+			bounds.extend(self.markers[i].position);
 		}
 		// Extend the boundaries of the map for each marker
 		map.fitBounds(bounds);
-		
-		function populateInfoWindow(marker, infowindow) {
-			
-			var fsPlaceUrl = "https://api.foursquare.com/v2/venues/search?ll=" 
-				+ marker.lat + "," + marker.lng
-				+ "&query=" + marker.title
-				+ "&client_id=" + fsClientID
-				+ "&client_secret=" + fsClientSecret
-				+ "&limit=1&v=20180609";
-			console.log(fsPlaceUrl);
-			// Check to make sure the infowindow is not already opened on this marker.
-			if (infowindow.marker != marker) {
-				infowindow.marker = marker;
-				$.getJSON(fsPlaceUrl, function(marker) {
-					var venueResponse = marker.response.venues[0];
-					var locName = venueResponse.name;
-					var locAddressLine1 = venueResponse.location.formattedAddress[0];
-					var locAddressLine2 = venueResponse.location.formattedAddress[1];
-					
-					infowindow.setContent('<div><h4>' + locName + '</h4>' 
-					+ '<h5>' + locAddressLine1 + '</h5><h5>' + locAddressLine2
-					+ '</h5><br><img src="img/Powered-by-Foursquare-full-color-300.png"></div>');
-				}).fail(function(e){
-					infowindow.setContent('<div>' + fsErr + '</div>');
-				});
-				infowindow.open(map, marker);
-				// Make sure the marker property is cleared if the infowindow is closed.
-				infowindow.addListener('closeclick',function() {
-					infowindow.setMarker = null;
-				});
-			}
-		}
 	}
-	
-	self.initMap();
+    
+    self.initMap();
+    
+    this.toggleLocItem = function() {
+        var thisMarker = self.markers[this.id];
+        if (thisMarker.getAnimation() !== null) {
+            thisMarker.setAnimation(null);
+        } else {
+            thisMarker.setAnimation(google.maps.Animation.BOUNCE);
+            //stop bounce animation after two cycles
+            setTimeout(function () {
+                thisMarker.setAnimation(null);
+            }, 1400);
+            self.populateInfoWindow(thisMarker, self.largeInfowindow);
+        }
+    }
+    
+    this.filteredLocList = ko.computed(function() {
+        var filteredList = [];
+        
+        for(var i = 0; i < self.markers.length; i++) {
+            if(self.markers[i].title.toLowerCase().includes(this.searchFilter().toLowerCase())) {
+                filteredList.push(new MapLocation(self.markers[i].title, i));
+                self.markers[i].setVisible(true);
+            }
+            //if there is no match with the search, hide this element
+            else {
+                self.markers[i].setVisible(false);
+            }
+        }
+        return filteredList;
+    }, this);
+
 }
 
 function mapAPIError() {
--- js/places.js
+++ js/places.js
@@ -1,7 +1,8 @@
 var places = [
-	{title: 'Atlantic Seafood Restaurant', location: {lat: 34.069660, lng: -118.133884}},
+	{title: 'Atlantic Seafood', location: {lat: 34.069660, lng: -118.133884}},
+    {title: 'Empress Harbor', location: {lat: 34.063005, lng: -118.134476}},
 	{title: 'Ocean Star', location: {lat: 34.064379, lng: -118.134531}},
-	{title: 'Capital Dim Sum', location: {lat: 34.063320, lng: -118.132902}},
+	{title: 'Capital Dim Sum & BBQ', location: {lat: 34.063320, lng: -118.132902}},
 	{title: 'NBC Seafood', location: {lat: 34.058779, lng: -118.133247}},
 	{title: 'Monterey Palace', location: {lat: 34.063257, lng: -118.109061}},
 	{title: 'Top Island Seafood', location: {lat: 34.078431, lng: -118.115362}},
